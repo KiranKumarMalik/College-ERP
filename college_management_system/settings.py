9cb05252ccf8e56716903579494412376c795677
KiranKumarMalik <malikkiran413@gmail.com>
2025-04-30 11:16:58 +0530

    new code added

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -157,7 +157,6 @@ TIME_ZONE = 'Asia/Kolkata'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-
 EMAIL_HOST_USER = "kirankumarmalik321@gmail.com"
 EMAIL_HOST_PASSWORD = "ykbz gxhn whjb jrha"
 EMAIL_USE_TLS = True
